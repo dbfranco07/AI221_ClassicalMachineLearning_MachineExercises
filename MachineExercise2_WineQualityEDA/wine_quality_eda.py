68327cc442f612461e37804f057f4467b7ea6fcf
unknown <dbfranco@up.edu.ph>
2024-09-11 19:12:32 +0800

    additional code

--- MachineExercise2_WineQualityEDA/wine_quality_eda.py
+++ MachineExercise2_WineQualityEDA/wine_quality_eda.py
@@ -1,18 +1,40 @@
 from ucimlrepo import fetch_ucirepo
+
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.preprocessing import StandardScaler
 
 wine_quality = fetch_ucirepo(id=186)
 
-X = wine_quality.data.features
-y = wine_quality.data.targets
+X = wine_quality.data.original
 
-# checking how many different quality values are there
-y.value_counts(dropna=False)
-
-# checking the shape of the features data
-X.shape
-
+# checking the column names of the data. This includes possible features and
+# target for machine learning
 X.columns
 
-dir(wine_quality)
-wine_quality.metadata
+# checking the shape of the data
+X.shape
+
+# Checking if there are any missing data
+X.info()
+
+# Quick checking of statistics of data
+# Adding parameter include='all' to also see statistics for color -- which is a 
+# categorical data
+X.describe(include='all')
+
+X['quality'].value_counts(normalize=True).plot(kind='bar')
+
+help(pd.Series.plot)
+
+# From the dataset, it seems that quality and color are an interesting target
+# for classification based on the other features
+
+sns.pairplot(X, hue='color')
+sns.pairplot(X, hue='quality')
+
+plt.figure(figsize=(15, 15))
+sns.heatmap(X.corr(numeric_only=True), annot=True)
